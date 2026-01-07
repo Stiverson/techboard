2444a5475948507ea8b151b7fee1c4a408dbdd05
Stiverson <stiverson.machado@gmail.com>
2026-01-07 13:35:37 -0300

    Atualização tela de temas e ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import { Banner } from './componentes/Banner'
 import { FormularioDeEvento } from './componentes/FormularioDeEvento'
 import { Tema } from './componentes/Tema'
 
 function App() {
 
   const temas = [
-
     {
         id: 1,
         nome: 'front-end'
@@ -37,9 +37,7 @@ function App() {
         <header>
             <img src='/logo.png' alt='' />
         </header>
-        <section>
-            <img src='/banner.png' alt=''/>
-        </section>
+        <Banner />
         <FormularioDeEvento />
         <section>
             <Tema tema={temas[0]}/>
